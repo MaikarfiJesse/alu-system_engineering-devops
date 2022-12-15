d0aa700f0f6adfbeb90f81de3b503fd4554500d9
MaikarfiJesse <j.maikarfi1@alustudent.com>
2022-12-15 00:20:16 +0000

    Strace is your friend

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # find and fix why apache is returning a 500 error 
 exec { 'fixing error...':
- onlyif  => 'test -e /var/www/html/wp-settings.php',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
